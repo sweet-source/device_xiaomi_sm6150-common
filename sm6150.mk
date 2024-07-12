b3ef19672f6172593bd8a0578b4168860e44d932
afterallafk <shivamatiet2001@gmail.com>
2024-07-13 00:51:21 +0530

    sm6150-common: lineage -> blaze
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -373,7 +373,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/blaze/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
