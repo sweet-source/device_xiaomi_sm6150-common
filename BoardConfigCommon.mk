693b5d07514a8237d3c3aa14cef26ec98d2e3894
kamleshhv <kamleshvansajalia@gmail.com>
2024-09-26 11:19:21 +0000

    sm6150-common: drop livedisplay
    This reverts commit 60373ed616b248ff2cfacc7ff0a1e713b4f2a921.
    
    Revert "sm6150-common: Replace writepid with task_profiles command for cgroup migration"
    
    This reverts commit 4395b79e9fdf8e45405b0185ccc81856e6fa4cfb.
    
    Revert "sm6150-common: livedisplay: Make supported interfaces configurable"
    
    This reverts commit 5d9b716f90f9ece724db59ba2ff075d13f0e2f1f.
    
    Revert "sm6150-common: livedisplay: Fix racy init"
    
    This reverts commit 4560af5d2fd2242ce3c1679e0b973dbc0d987c5d.
    
    Revert "sm6150-common: livedisplay: Use modules from common implementation"
    
    This reverts commit 5848b702e0b4a46c7d12975b8e8b57ab6a88ee16.
    
    Revert "sm6150-common: livedisplay: Uprev to 2.1"
    
    This reverts commit 6d05622ab173907c41377541aa61a106cd5a0a00.
    
    Revert "sm6150-common: livedisplay: Renamed AntiFlicker node"
    
    This reverts commit afb4a21c3fb2480591121850bfe72460217c1115.
    
    Revert "sm6150-common: livedisplay: Add anti flicker support"
    
    This reverts commit 83f9791ee29335e30e2f87853ff95643badd0baf.
    
    sm6150-common: drop livedisplay

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -156,13 +156,10 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
-# Soong
-SOONG_CONFIG_NAMESPACES += xiaomiSm6150Vars
-SOONG_CONFIG_xiaomiSm6150Vars += \
-    livedisplay_support_anti_flicker \
-    livedisplay_support_sunlight_enhancement
-SOONG_CONFIG_xiaomiSm6150Vars_livedisplay_support_anti_flicker ?= true
-SOONG_CONFIG_xiaomiSm6150Vars_livedisplay_support_sunlight_enhancement ?= true
+# Trust
+TARGET_TRUST_USB_CONTROL_PATH := /sys/devices/platform/soc/a600000.ssusb/usb_data_enabled
+TARGET_TRUST_USB_CONTROL_ENABLE := 0
+TARGET_TRUST_USB_CONTROL_DISABLE := 1
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
